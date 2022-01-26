b0de7b55a47905af1a0d7864a28c2fc4b3085892
Joe Fisher <fisher.joe@gmail.com>
2022-01-26 07:28:06 -0600

    Rough sketch of solution in comments.

--- alphabet-cipher/main.cpp
+++ alphabet-cipher/main.cpp
@@ -1,6 +1,20 @@
 #include <iostream>
 
-int main() {
+int main(int argc, char ** argv) {
+
+    // get arguments: options for secret phrase, message
+    //      (use std::find() like here: https://stackoverflow.com/questions/865668/parsing-command-line-arguments-in-c
+    // sanitize and normalize secret phrase & message (remove spaces, lower case)
+    // repeat letters of secret phrase over and over again until same size as message
+    //      (handle case where message is smaller than secret phrase)
+    // for i in message.size:
+    //      schar = secret_phrase[i]
+    //      mchar = message[i]
+    //      encrypted_message += lookup[mchar][schar]
+    // return encrypted_message
+    // TODO add decrypting as well
+
     std::cout << "Hello, World!" << std::endl;
+
     return 0;
 }
