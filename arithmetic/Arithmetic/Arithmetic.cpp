1bdee47f7755c063699363635568c23d70828d61
Joe Fisher <fisher.joe@gmail.com>
2022-02-16 12:40:24 -0600

    Started rough sketch of solution in comments.

--- arithmetic/Arithmetic/Arithmetic.cpp
+++ arithmetic/Arithmetic/Arithmetic.cpp
@@ -6,8 +6,17 @@ auto Arithmetic::Do(const std::string& expression) -> int {
     // should error out if terms encountered before parentheses...
     // default (if there's nothing to do) to 0
 
-    // start with simple case: (1 + 1)
-    //      follows form: (operand operator operand)
+    // General design of solution (I think):
+    // Grammar roughly follows the rules (not counting whitespace):
+    //      Expr: ( Expr|Symb Op Expr|Symb ) | ()
+    //      Op: + | - | * | / | <null>
+    //      Symb: -?[0-9]+ | <null>
+    // Use recursion to evaluate Expr
+    // (since recursion is needed, we can't parse with regex!)
+    // 1. Tokenize input string into vector of strings
+    // 2. Token state machine: ExpectOpenParen -> ExpectSymbOrOpenParens -> ExpectOp    ExpectCloseParen
+    //                                            |__^   |  ^_________________|  ^_________|   ^  |__^
+    //                                                   |_____________________________________|
 
     return 0;
 }
