63c7fd7578157d9c08c7da9e48da2132f5e821d5
Joe Fisher <fisher.joe@gmail.com>
2022-02-18 19:10:42 -0600

    Committing garbage in the hopes that we can clean it up...

--- arithmetic/Arithmetic/Arithmetic.cpp
+++ arithmetic/Arithmetic/Arithmetic.cpp
@@ -7,16 +7,138 @@ auto Arithmetic::Do(const std::string& expression) -> int {
     // default (if there's nothing to do) to 0
 
     // General design of solution (I think):
+    //      * Every open paren has to be matched with a close paren _sometime_.
+    //          - Any time we encounter an open paren, we call the function recursively
+    //          - Any time we encounter a closed paren, we return
+    //      * In the function:
+    //          - Current result is passed in
+    //          - If input == '(', recurse
+    //          - If input == ')', return result
+    //          - If input == Num, push Num and expect Op next
+    //          - If input == Op and Op is expected, save Op and expect Num next
+    //          - If input == Num and Num expected, do math
+    //      * Operations have to be surrounded by numbers or parens
+    //      *
     // Grammar roughly follows the rules (not counting whitespace):
+    //      Num -> -?[0-9]+
+    //      Op -> [+-*/]
+    //      Expr -> ()
+    //      Expr -> (Num)
+    //      Expr -> (Num Op Num)
+    //      Expr -> (Expr Op Num)
+    //      Expr -> (Num Op Expr)
+    //      Expr ->
     //      Expr: ( Expr|Symb Op Expr|Symb ) | ()
-    //      Op: + | - | * | / | <null>
-    //      Symb: -?[0-9]+ | <null>
+    //      Op   -> [+-*/]
+    //      Num  -> -?[0-9]+
+    //      Expr -> (Expr|Num Op Expr|Num )
     // Use recursion to evaluate Expr
     // (since recursion is needed, we can't parse with regex!)
     // 1. Tokenize input string into vector of strings
     // 2. Token state machine: ExpectOpenParen -> ExpectSymbOrOpenParens -> ExpectOp    ExpectCloseParen
     //                                            |__^   |  ^_________________|  ^_________|   ^  |__^
     //                                                   |_____________________________________|
+
+//    int result = 0;
+//
+//    // PEMDAS: P
+//    auto symbolLocation = expression.find('(');
+//    if (symbolLocation != std::string::npos) {
+//        result = Do(expression.substr(symbolLocation));
+//    }
+//
+//    // PEMDAS: MD
+//    do {
+//        symbolLocation = expression.find('*');
+//        auto secondSymbolLocation = expression.find('/');
+//
+//        if (symbolLocation == std::string::npos && secondSymbolLocation == std::string::npos) {
+//            break;
+//        }
+//
+//        if (symbolLocation < secondSymbolLocation) {
+//            // do M
+//            // find operands on either side
+//            auto leftOperand = Arithmetic::GetLeftOperand(symbolLocation);
+//            auto rightOperand = Arithmetic::GetRightOperand(symbolLocation);
+//            // do math
+//            int result = leftOperand * rightOperand;
+//            // replace operands and operator with result
+//        } else {
+//            // do D
+//            // find operands on either side
+//            auto leftOperand = Arithmetic::GetLeftOperand(secondSymbolLocation);
+//            auto rightOperand = Arithmetic::GetRightOperand(secondSymbolLocation);
+//            // do math
+//            int result = leftOperand / rightOperand;
+//            // replace operands and operator with result
+//        }
+//    }
+
+    return 0;
+}
+
+int Arithmetic::GetLeftOperand(unsigned long long int location) {
+    return 0;
+}
+
+int Arithmetic::GetRightOperand(unsigned long long int location) {
+    return 0;
+}
+
+auto Arithmetic::Calculate(std::vector<std::string>& tokens, int start, int end) -> int { // NOLINT(misc-no-recursion)
+    // PEMDAS: P
+    // take care of parentheses
+    for (int i = start; i < end; i++) {
+        if (tokens[i] == ")") {
+            for (int j = i-1; j >= start; j--) {
+                if (tokens[j] == "(") {
+                    auto result = Arithmetic::Calculate(tokens, j+1, i);
+                    tokens.erase(tokens.begin()+j, tokens.begin()+i+1);
+                    tokens.insert(tokens.begin()+i, std::to_string(result));
+                    end -= i - j + 1;
+                }
+            }
+        }
+    }
+
+    // PEMDAS: MD
+    // search whole token vector for * & /
+    for (auto token : tokens) {
+        if (token == "*" || token == "/") {
+            if (token == "*") {
+                // get operands to left and right
+                // do *
+                // replace operands & operator with result in token vector
+            } else {
+                // get operands to left and right
+                // do /
+                // replace operands & operator with result in token vector
+            }
+        }
+    }
+
+    // PEMDAS: AS
+    // search whole token vector for + & -
+    for (auto token : tokens) {
+        if (token == "+" || token == "-") {
+            if (token == "+") {
+                // get operands to left and right
+                // do +
+                // replace operands & operator with result in token vector
+            } else {
+                // get operands to left and right
+                // do -
+                // replace operands & operator with result in token vector
+            }
+        }
+    }
+
+    // PEMDAS: P
+    // There should only be a single closing paren left in token vector, along with answer
+    // Do error checking and return result
+    // TODO need to remove rest of stuff from token vector? Need to return overall token vector?
+
 
     return 0;
 }
--- arithmetic/Arithmetic/Arithmetic.h
+++ arithmetic/Arithmetic/Arithmetic.h
@@ -1,6 +1,13 @@
 #include <string>
+#include <vector>
 
 class Arithmetic {
 public:
     auto Do(const std::string& expression) -> int;
+
+    int GetLeftOperand(unsigned long long int location);
+
+    int GetRightOperand(unsigned long long int location);
+
+    auto Calculate(std::vector<std::string>& tokens, int start, int end) -> int;
 };
