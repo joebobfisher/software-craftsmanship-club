1b57b6484d4d337a5da579a3a8d54508c85e7f37
Joe Fisher <fisher.joe@gmail.com>
2022-02-20 01:15:19 -0600

    Cleaned up a bit, but still looks garbage-y. Have some ideas for improvement though!

--- arithmetic/Arithmetic/Arithmetic.cpp
+++ arithmetic/Arithmetic/Arithmetic.cpp
@@ -1,24 +1,35 @@
 #include "Arithmetic.h"
 
 auto Arithmetic::Calculate(const std::string& expression) -> double {
-    //std::vector<std::string> v {"(","(","1","+","2",")","*","(","3","*","4",")",")"};
-    //std::vector<std::string> v {"(", "1", "+", "(", "(", "2", "+", "3", ")", "*", "(","4", "*", "5",")", ")", ")"};
-    //std::vector<std::string> v {"(", "5", "*", "(", "4", "*", "(", "3", "*", "(", "2", "*", "(", "1", "*", "9", ")", "/", "8", "-", "7", ")", "+", "6", ")", ")", ")"};
-    //std::vector<std::string> v {"(","(","(",")","(",")",")",")"};
-
     // TODO Tokenize the expression string
+    std::vector<std::string> tokenizedExpression = Tokenize(expression);
+//    tokenizedExpression = {"(","(","1","+","2",")","*","(","3","*","4",")",")"};
+//    tokenizedExpression = {"(", "1", "+", "(", "(", "2", "+", "3", ")", "*", "(","4", "*", "5",")", ")", ")"};
+//    tokenizedExpression = {"(", "5", "*", "(", "4", "*", "(", "3", "*", "(", "2", "*", "(", "1", "*", "9", ")", "/", "8", "-", "7", ")", "+", "6", ")", ")", ")"};
+//    tokenizedExpression = {"(","(","(",")","(",")",")",")"};
+
     // TODO return error if first token not "("
 
-    Reduce(v, 0, v.size());
-    auto result = v.size() > 0 ? std::stod(v[0]) : 0;
-    return result;
+    Reduce(tokenizedExpression, 0, tokenizedExpression.size());
+    return !tokenizedExpression.empty() ? std::stod(tokenizedExpression[0]) : 0;
 }
 
-void Arithmetic::Reduce(std::vector<std::string>& tokens, int start, int end) { // NOLINT(misc-no-recursion)
-    // PEMDAS: P
-    // take care of parentheses
-    for (int i = start; i < end; i++) {
+std::vector<std::string> Arithmetic::Tokenize(const std::string &basicString) {
+    return {};
+}
+
+void Arithmetic::Reduce(std::vector<std::string>& tokens, size_t start, size_t end) { // NOLINT(misc-no-recursion)
+    // PEMDAS
+    HandleParentheses(tokens, start, end);
+    HandleMultiplyAndDivide(tokens, start, end);
+    HandleAddAndSubtract(tokens, start, end);
+}
+
+// TODO passing around end as a reference is bad -- side effects, and state/context! Better if we passed sub-vectors (then we wouldn't need to pass start & end!)
+void Arithmetic::HandleParentheses(std::vector<std::string> &tokens, size_t start, size_t &end) { // NOLINT(misc-no-recursion)
+    for (auto i = start; i < end; i++) {
         if (tokens[i] == ")") {
+            // TODO could maybe be extracted as a "handle matching open paren" function (or maybe just functions to find i & j???)
             for (int j = i-1; j >= start; j--) {
                 if (tokens[j] == "(") {
                     // remove the matching parens from the vector
@@ -26,7 +37,7 @@ void Arithmetic::Reduce(std::vector<std::string>& tokens, int start, int end) { 
                     tokens.erase(tokens.begin()+j);
 
                     // Handle what was between the parens
-                    Arithmetic::Reduce(tokens, j, i-1);
+                    Reduce(tokens, j, i-1);
 
                     // probably more efficient way of doing this, but this is foolproof...
                     end = static_cast<int>(tokens.size());
@@ -37,71 +48,42 @@ void Arithmetic::Reduce(std::vector<std::string>& tokens, int start, int end) { 
             }
         }
     }
+}
 
-    // PEMDAS: MD
-    // search whole token vector for * & /
-    for (int i = start; i < end; i++) {
+void Arithmetic::HandleMultiplyAndDivide(std::vector<std::string> &tokens, size_t start, size_t &end) {
+    for (auto i = start; i < end; i++) {
         if (tokens[i] == "*" || tokens[i] == "/") {
             if (tokens[i] == "*") {
-                // get operands to left and right
-                auto left = std::stod(tokens[i-1]);
-                auto right = std::stod(tokens[i+1]);
-
-                // do *
-                auto result = left * right;
-
-                // replace operands & operator with result in token vector
-                tokens.erase(tokens.begin()+i-1, tokens.begin()+i+2);
-                tokens.insert(tokens.begin()+i-1, std::to_string(result));
-                end -= 2;
-                i -= 1;
+                DoMathAndReduceToAnswer(tokens, end, i, [](double x, double y) -> double { return x * y; } );
             } else {
-                // get operands to left and right
-                auto left = std::stod(tokens[i-1]);
-                auto right = std::stod(tokens[i+1]);
-
-                // do /
-                auto result = left / right;
-
-                // replace operands & operator with result in token vector
-                tokens.erase(tokens.begin()+i-1, tokens.begin()+i+2);
-                tokens.insert(tokens.begin()+i-1, std::to_string(result));
-                end -= 2;
-                i -= 1;
+                DoMathAndReduceToAnswer(tokens, end, i, [](double x, double y) -> double { return x / y; } );
             }
         }
     }
+}
 
-    // PEMDAS: AS
-    // search whole token vector for + & -
-    for (int i = start; i < end; i++) {
+void Arithmetic::DoMathAndReduceToAnswer(std::vector<std::string> &tokens, size_t &end, size_t &i, std::function<double (double, double)> math) {
+    // get operands to left and right
+    auto left = std::stod(tokens[i-1]);
+    auto right = std::stod(tokens[i+1]);
+
+    // do whatever math function you were given
+    auto result = math(left, right);
+
+    // replace operands & operator with result in token vector
+    tokens.erase(tokens.begin()+i-1, tokens.begin()+i+2);
+    tokens.insert(tokens.begin()+i-1, std::to_string(result));
+    end -= 2;
+    i -= 1;
+}
+
+void Arithmetic::HandleAddAndSubtract(std::vector<std::string> &tokens, size_t start, size_t &end) {
+    for (auto i = start; i < end; i++) {
         if (tokens[i] == "+" || tokens[i] == "-") {
             if (tokens[i] == "+") {
-                // get operands to left and right
-                auto left = std::stod(tokens[i-1]);
-                auto right = std::stod(tokens[i+1]);
-
-                // do +
-                auto result = left + right;
-
-                // replace operands & operator with result in token vector
-                tokens.erase(tokens.begin()+i-1, tokens.begin()+i+2);
-                tokens.insert(tokens.begin()+i-1, std::to_string(result));
-                end -= 2;
-                i -= 1;
+                DoMathAndReduceToAnswer(tokens, end, i, [](double x, double y) -> double { return x + y; } );
             } else {
-                // get operands to left and right
-                auto left = std::stod(tokens[i-1]);
-                auto right = std::stod(tokens[i+1]);
-
-                // do -
-                auto result = left - right;
-
-                // replace operands & operator with result in token vector
-                tokens.erase(tokens.begin()+i-1, tokens.begin()+i+2);
-                tokens.insert(tokens.begin()+i-1, std::to_string(result));
-                end -= 2;
-                i -= 1;
+                DoMathAndReduceToAnswer(tokens, end, i, [](double x, double y) -> double { return x - y; } );
             }
         }
     }
--- arithmetic/Arithmetic/Arithmetic.h
+++ arithmetic/Arithmetic/Arithmetic.h
@@ -1,8 +1,17 @@
 #include <string>
 #include <vector>
+#include <functional>
 
 class Arithmetic {
 public:
-    auto Calculate(const std::string& expression) -> double;
-    void Reduce(std::vector<std::string>& tokens, int start, int end);
+    auto Calculate(const std::string &expression) -> double;
+
+private:
+    auto Tokenize(const std::string &basicString) -> std::vector<std::string>;
+    void Reduce(std::vector<std::string> &tokens, size_t start, size_t end);
+    void HandleParentheses(std::vector<std::string> &tokens, size_t start, size_t &end);
+    static void HandleMultiplyAndDivide(std::vector<std::string> &tokens, size_t start, size_t &end) ;
+    static void HandleAddAndSubtract(std::vector<std::string> &tokens, size_t start, size_t &end) ;
+
+    static void DoMathAndReduceToAnswer(std::vector<std::string> &tokens, size_t &end, size_t &i, std::function<double (double, double)> math);
 };
