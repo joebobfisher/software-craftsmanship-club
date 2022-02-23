b4965d4b8c32ab34b2a8ad2708cc1d8acdae4050
Joe Fisher <fisher.joe@gmail.com>
2022-02-23 12:51:29 -0600

    Adding clang-tidy pragmas, so we don't get a bunch of false-positives.

--- tiny-maze/google_tests/MazeTests.cpp
+++ tiny-maze/google_tests/MazeTests.cpp
@@ -1,6 +1,16 @@
-#include "gtest/gtest.h"
-#include "Maze.h"
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Wunknown-pragmas"
+#pragma ide diagnostic ignored "cppcoreguidelines-avoid-non-const-global-variables"
+#pragma ide diagnostic ignored "cppcoreguidelines-owning-memory"
+#pragma ide diagnostic ignored "cert-err58-cpp"
+#pragma ide diagnostic ignored "cppcoreguidelines-avoid-goto"
+#pragma ide diagnostic ignored "cppcoreguidelines-non-private-member-variables-in-classes"
 
-TEST(Succeeds) {
-    SUCCESS();
+#include "gtest/gtest.h"
+#include "../Maze/Maze.h"
+
+TEST(MazeTests, Succeeds) {
+    SUCCEED();
 }
+
+#pragma clang diagnostic pop
