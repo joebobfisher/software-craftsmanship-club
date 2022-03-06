a6f931e4cc964ee7f7cd8c0a6860cff8482a311a
Joe Fisher <fisher.joe@gmail.com>
2022-03-06 11:19:55 -0600

    Revert "Added "Maze" class. In the middle of incorporating it with RouteFinder, so code currently doesn't build -- but all tests for Maze pass. See TODOs."
    This reverts commit 8bff3ce2b8370281d41f54c4650ba4f761d675d7.

--- tiny-maze/RouteFinder/RouteFinder.cpp
+++ tiny-maze/RouteFinder/RouteFinder.cpp
@@ -1,41 +1,37 @@
 #include "RouteFinder.h"
 
-RouteFinder::RouteFinder(Maze& maze) : MazeToSearch(maze) {}
+RouteFinder::RouteFinder(std::vector<std::vector<char>>& maze) : Maze(maze) {}
 
 void RouteFinder::FindRoute() {
-//    InitializeVisitedNodes();
-//    InitializeRouteStack();
-    MazeToSearch.InitializeMaze();
+    InitializeVisitedNodes();
+    InitializeRouteStack();
+
     FindRouteDfs(0, 0);
 }
 
-//void RouteFinder::InitializeVisitedNodes() {
-//    VisitedNodes = {};
-//
-//    for (auto & col : Maze) {
-//        std::vector<bool> column;
-//        for (int j = 0; j < col.size(); j++) {
-//            column.push_back(false); // NOLINT not pre-allocating is fine
-//        }
-//        VisitedNodes.push_back(column);
-//    }
-//}
-//
-//void RouteFinder::InitializeRouteStack() { RouteStack = {}; }
+void RouteFinder::InitializeVisitedNodes() {
+    VisitedNodes = {};
+
+    for (auto & col : Maze) {
+        std::vector<bool> column;
+        for (int j = 0; j < col.size(); j++) {
+            column.push_back(false); // NOLINT not pre-allocating is fine
+        }
+        VisitedNodes.push_back(column);
+    }
+}
+
+void RouteFinder::InitializeRouteStack() { RouteStack = {}; }
 
 auto RouteFinder::FindRouteDfs(int x, int y) -> bool { // NOLINT(misc-no-recursion, readability-identifier-length)
-    //if (!IsValidNode(x, y) || VisitedNodes[x][y]) {
-    if (!MazeToSearch.IsVisitableNode(x, y) || MazeToSearch.IsVisitedNode(x, y)) {
+    if (!IsValidNode(x, y) || VisitedNodes[x][y]) {
         return false;
     }
 
-//    VisitedNodes[x][y] = true;
-//    RouteStack.emplace_back(x, y);
-    MazeToSearch.VisitNode(x, y);
-    MazeToSearch.AddNodeToRoute(x, y);
+    VisitedNodes[x][y] = true;
+    RouteStack.emplace_back(x, y);
 
-    //if (Maze[x][y] == 'E') {
-    if (MazeToSearch.IsTheEnd(x, y)) {
+    if (Maze[x][y] == 'E') {
         return true;
     }
 
@@ -46,21 +42,20 @@ auto RouteFinder::FindRouteDfs(int x, int y) -> bool { // NOLINT(misc-no-recursi
         }
     }
 
-//    RouteStack.pop_back();
-    MazeToSearch.RemoveLastNodeFromRoute();
+    RouteStack.pop_back();
 
     return false;
 }
 
-//auto RouteFinder::IsValidNode(int x, int y) -> bool {   // NOLINT(readability-identifier-length)
-//    return x >= 0 && x < Maze.size() && y >= 0 && y < Maze[x].size() && Maze[x][y] != '1';
-//}
-//
-//auto RouteFinder::GetVisitedList() -> std::vector<std::vector<bool>> {
-//    return VisitedNodes;
-//}
-//
-//auto RouteFinder::GetRouteStack() -> std::vector<std::pair<int, int>> {
-//    return RouteStack;
-//}
+auto RouteFinder::IsValidNode(int x, int y) -> bool {
+    return x >= 0 && x < Maze.size() && y >= 0 && y < Maze[x].size() && Maze[x][y] != '1';
+}
+
+auto RouteFinder::GetVisitedList() -> std::vector<std::vector<bool>> {
+    return VisitedNodes;
+}
+
+auto RouteFinder::GetRouteStack() -> std::vector<std::pair<int, int>> {
+    return RouteStack;
+}
 
--- tiny-maze/RouteFinder/RouteFinder.h
+++ tiny-maze/RouteFinder/RouteFinder.h
@@ -2,23 +2,23 @@
 #define TINY_MAZE_ROUTEFINDER_H
 
 
-#include "Maze.h"
+#include <vector>
 
 class RouteFinder {
 public:
-    explicit RouteFinder(Maze& maze);
+    explicit RouteFinder(std::vector<std::vector<char>>& maze);
     void FindRoute();
-//    void InitializeVisitedNodes();
-//    void InitializeRouteStack();
+    void InitializeVisitedNodes();
+    void InitializeRouteStack();
     auto FindRouteDfs(int x, int y) -> bool;    // NOLINT(readability-identifier-length)
-//    auto IsValidNode(int x, int y) -> bool;     // NOLINT(readability-identifier-length)
-//    auto GetVisitedList() -> std::vector<std::vector<bool>>;
-//    auto GetRouteStack() -> std::vector<std::pair<int, int>>;
+    auto IsValidNode(int x, int y) -> bool;     // NOLINT(readability-identifier-length)
+    auto GetVisitedList() -> std::vector<std::vector<bool>>;
+    auto GetRouteStack() -> std::vector<std::pair<int, int>>;
 
 private:
-    Maze& MazeToSearch;
-//    std::vector<std::vector<bool>> VisitedNodes;
-//    std::vector<std::pair<int, int>> RouteStack;
+    std::vector<std::vector<char>> Maze;
+    std::vector<std::vector<bool>> VisitedNodes;
+    std::vector<std::pair<int, int>> RouteStack;
 };
 
 
