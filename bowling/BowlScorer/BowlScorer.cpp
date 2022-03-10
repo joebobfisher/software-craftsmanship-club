0ca8d95e8de6a2521e0b08781b2803a21ef925b6
Joe Fisher <fisher.joe@gmail.com>
2022-03-10 08:31:46 -0600

    Parsed string by whitespace.

--- bowling/BowlScorer/BowlScorer.cpp
+++ bowling/BowlScorer/BowlScorer.cpp
@@ -1,5 +1,21 @@
 #include "BowlScorer.h"
+#include <vector>
+#include <regex>
+
+// take list of whitespace-delimited pairs of rolls
+// tokenize list of rolls
+// last frame could have 3 rolls in it if an X or / is in it
+// valid values: 0-9, x, or /
 
 auto BowlScorer::Score(std::string frameString) -> int {
+    // split string by whitespace
+    // TODO separate this out into a class you can use again
+    // (this is going to come up again, and it's easier than using Boost)
+    std::regex regex("\\s+");
+    std::sregex_token_iterator iterator(frameString.begin(), frameString.end(), regex, -1);
+    std::vector<std::string> frames{iterator, {}};
+
+
+
     return 0;
 }
--- bowling/main.cpp
+++ bowling/main.cpp
@@ -2,10 +2,6 @@
 #include "BowlScorer/BowlScorer.h"
 
 auto main() -> int {
-    // take list of whitespace-delimited pairs of rolls
-    // tokenize list of rolls
-    // last frame could have 3 rolls in it if an X or / is in it
-    // valid values: 0-9, x, or /
     std::string frames = "12 34 54 63 72 81 X 1/ 27 5/9";
 
     BowlScorer bowlScorer;
