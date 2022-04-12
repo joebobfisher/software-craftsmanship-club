f6a100a00cb62d5334e63d8fd191a20617911e86
Joe Fisher <joe.fisher@agcocorp.com>
2022-04-12 10:36:21 -0500

    Deciding not to abstract away the rules list from RuleChecker constructor via DI, since we might want to reuse the same rule list again in the future (shouldn't have to write down all the rules again just to use the RuleChecker).

--- password/Rules/Rule/Rule.h
+++ password/Rules/Rule/Rule.h
@@ -4,7 +4,7 @@
 
 #include "PasswordObject.h"
 
-class Rule {
+class Rule {    // NOLINT(cppcoreguidelines-special-member-functions) Does Rule of 5 apply to interfaces???
 public:
     virtual void Check(const PasswordObject& password) = 0;
     virtual ~Rule() = default;      // Important here since we're `new`ing stuff in the rule checker
