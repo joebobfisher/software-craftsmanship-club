f15df9f8979b25b4b9a192a0207770ae385bc57a
Joe Fisher <joe.fisher@agcocorp.com>
2022-04-20 13:13:55 -0500

    Adding google test framework.

---
+++ rover-redux/Rover/Rover.h
@@ -0,0 +1,10 @@
+#ifndef ROVER_REDUX_ROVER_H
+#define ROVER_REDUX_ROVER_H
+
+
+class Rover {
+
+};
+
+
+#endif //ROVER_REDUX_ROVER_H
---
+++ rover-redux/google_tests/RoverTests.cpp
@@ -0,0 +1,20 @@
+#include "gtest/gtest.h"
+#include "Rover.h"
+
+class RoverTest : public ::testing::Test
+{
+protected:
+    void SetUp() override;
+
+    Rover target;
+};
+
+void RoverTest::SetUp()
+{
+
+}
+
+TEST_F(RoverTest, Dummy) // NOLINT(cert-err58-cpp)
+{
+    SUCCEED();
+}
